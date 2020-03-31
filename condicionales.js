354882f4269d2350cd384afd725972797e4a23bb
Rosa <rosavergarab@gmail.com>
2020-03-31 18:53:32 -0500

    ejericio de los condicionales

---
+++ condicionales.js
@@ -0,0 +1,11 @@
+var precio=200000;
+var cantidad=3;
+var smlv=877803;
+var total_venta=precio * cantidad;
+console.log("El precio total de la venta es "+total_venta);
+var descuento=0.2;
+if (total_venta > smlv)
+{
+	console.log("El valor del descuento es: "+(total_venta*descuento));
+	console.log("El precio de venta con descuento es: "+(total_venta-(total_venta*descuento)));
+};
