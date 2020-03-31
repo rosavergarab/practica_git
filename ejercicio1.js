aef1f7ce56802d096e542bc8281de5a066be9e02
Rosa <rosavergarab@gmail.com>
2020-03-31 18:38:54 -0500

    commit del resto de ejercicios

---
+++ ejercicio1.js
@@ -0,0 +1,7 @@
+var precio=20;
+var cantidad=10;
+var total_venta=precio * cantidad;
+console.log("El precio total de la venta es "+total_venta);
+var descuento=0.2;
+console.log("El valor del descuento es: "+(total_venta*descuento));
+console.log("El precio de venta con descuento es: "+(total_venta-(total_venta*descuento)));
---
+++ server.js
@@ -0,0 +1,7 @@
+/* Esta es una prueba
+de varias lineas
+de comentarios */
+// esta es una sola linea de comentarios
+
+console.log("Hola Mundo!");
+console.log("coronavirus te odio");
